32a49d4baea3cda90fb83751f42252003203a35f
Mathsiqueir <107413302+Mathsiqueir@users.noreply.github.com>
2024-02-13 20:10:47 -0300

    g

--- JS/js-moderno/22-webpack/webpack.config.js
+++ JS/js-moderno/22-webpack/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path') //modulo para trabalhar com caminhos
+
 
 module.exports = {
     entry:{
---
+++ Revisão/1-exercicio/index.js
@@ -0,0 +1,12 @@
+
+
+const nome = prompt("Qual o seu nome?")
+const lastName = prompt("Qual o seu sobrenome?")
+const study = prompt("Qual sua área de estudos")
+const born = prompt("Qual seu ano de nascimento?")
+
+
+alert(`Recruta registrado com sucesso! \n
+nome completo: ${nome} ${lastName}\n
+campo de estudo: ${study}\n
+Idade: ${2024 - born}`)
